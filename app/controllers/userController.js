cdcd8463a00c59f25cd57311b93aebcdecd17030
emmanuellopezsalda <emmanuellopezsalda123@gmail.com>
2024-09-06 17:06:03 -0500

    SPRINT 3

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -180,3 +180,32 @@ export const actualizarTiempoIntentos = (req, res) => {
         error(req, res, 500, "Error actualizando el tiempo y los intentos");
     }
 }
+export const crearGrupo = async(req, res) => {
+    const {nombre, desc} = req.body;
+    try {
+        const request = await basedatos.query("CALL SP_INSERTAR_GRUPO(?, ?)", [nombre, desc]);
+        success(req, res, 201, "Grupo creado")
+    } catch (err) {
+        console.error(err);
+        error(req, res, 500, "Error creando grupo");
+    }
+}
+export const obtenerGrupo = async(req, res) => {
+    try {
+        const request = await basedatos.query("CALL SP_OBTENER_ÚLTIMO_GRUPO()");
+        success(req, res, 201, request[0][0])
+    } catch (err) {
+        console.error(err);
+        error(req, res, 500, "Error creando grupo");
+    }
+}
+export const addParticipantes = async (req, res) => {
+    const { correo, grupo } = req.body;
+    try {
+        const request = await basedatos.query("CALL SP_ADD_INTEGRANTE(?, ?)", [correo, grupo]);
+        success(req, res, 201, "Integrante añadido correctamente");
+    } catch (err) {
+        console.error(err);
+        error(req, res, 500, "Error añadiendo integrante");
+    }
+}
--- app/routes/routes.user.js
+++ app/routes/routes.user.js
@@ -1,11 +1,11 @@
 import Router from "express";
-import { actualizarPoliticasSeguridad, actualizarTiempoIntentos, asignarRolUsuario, bloquearUsuario, crearUsuario, listarPoliticasSeguridad, listarSesiones, listarUser, logueoUsuario, registroInicioSesión } from "../controllers/userController.js";
+import { actualizarPoliticasSeguridad, actualizarTiempoIntentos, addParticipantes, asignarRolUsuario, bloquearUsuario, crearGrupo, crearUsuario, listarPoliticasSeguridad, listarSesiones, listarUser, logueoUsuario, obtenerGrupo, registroInicioSesión } from "../controllers/userController.js";
 
 const userRout = Router();
 
 userRout.get("/", listarUser);
 userRout.post("/", crearUsuario);
-userRout.get("/inicios", listarSesiones)
+userRout.get("/inicios", listarSesiones);
 userRout.post('/asignar-rol', asignarRolUsuario);
 userRout.post('/login', logueoUsuario);
 userRout.post("/historial-sesion", registroInicioSesión);
@@ -13,4 +13,7 @@ userRout.put("/estado/:id", bloquearUsuario);
 userRout.put("/actualizar-politicas", actualizarPoliticasSeguridad);
 userRout.get("/listar-politicas", listarPoliticasSeguridad);
 userRout.put("/actualizar-tiempo", actualizarTiempoIntentos);
+userRout.post("/crear-grupo", crearGrupo);
+userRout.post("/add-integrante", addParticipantes)
+userRout.get("/ultimo-grupo", obtenerGrupo);
 export default userRout;
