b74054dbf114ef58ed64724ee7d75f53ec1ea146
JnSsanRico <juan0096150@gmail.com>
2024-09-15 18:27:04 -0500

    respaldo de datos de usuario

--- app/routes/routes.admin.js
+++ app/routes/routes.admin.js
@@ -1,14 +1,18 @@
-import {Router} from "express";
+import { Router } from "express";
 import { verifyToken } from "../middlewares/oauth.js";
-import { exportDatabase } from "../controllers/exportdbController.js";
-import { generateCSR, getAllCertificates, renewCertificate } from "../controllers/certificateController.js";
+import { backupDatabase, restoreDatabase, listBackups, listUserBackups, backupUserData, restoreUserData } from "../controllers/backupController.js";
+import { getAllCertificates, renewCertificate } from "../controllers/certificateController.js";
 
 const rutaAdmin = Router();
 
 rutaAdmin.get("/token", verifyToken);
-rutaAdmin.get('/export-database', exportDatabase);
+rutaAdmin.get('/user-backups', listUserBackups);
+rutaAdmin.post('/user-backup', backupUserData);
+rutaAdmin.post('/restore-users', restoreUserData);
+rutaAdmin.get('/backups', listBackups);
+rutaAdmin.post('/backup', backupDatabase);
+rutaAdmin.post('/restore', restoreDatabase);
 rutaAdmin.get('/certificates', getAllCertificates);
 rutaAdmin.post('/certificates/renew/:id', renewCertificate);
-
 
 export default rutaAdmin;
