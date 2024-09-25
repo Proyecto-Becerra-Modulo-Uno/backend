940d8889b4928dc2b02f0b86bd10781826cb9fc9
333mari <marinela.rodriguez@misena.edu.co>
2024-09-24 21:12:51 -0500

    actualizacion de permisos especificos en los modulos

--- app/controllers/controller.modulos.permisos.js
+++ app/controllers/controller.modulos.permisos.js
@@ -13,16 +13,46 @@ export const GETModulosYpermisos = async (req, res) => {
   }
 };
 
-export const ActualizarEstado = async (req, res) => {
-    const { modulo, idPermiso, estado } = req.body; 
-    try {
-        await basedatos.query("CALL ACTUALIZAR_ESTADO(?, ?, ?)", [modulo, idPermiso, estado]);
-
-        res.json({ success: true, message: "Permiso actualizado correctamente" });
-    } catch (error) {
-        console.error("Error al cambiar el estado del permiso:", error);
-        res.status(500).json({ message: "Error interno del servidor" });
-    }
+export const GETpermisos = async (req, res) => {
+  try {
+    const respuesta = await basedatos.query(
+      "CALL SP_LISTAR_PERMISOS();"
+    );
+    success(req, res, 200, respuesta[0][0]);
+  } catch (err) {
+    error(req, res, 200, err || "Error interno del servidor");
+  }
 };
+
+export const ActualizarEstado = async (req, res) => {
+  const { id, idPermiso, estado } = req.body;
+  console.log(id);
+  console.log(idPermiso);
+  console.log(estado);
+
+  if (!id || !idPermiso || !estado) {
+    return res.status(400).json({ success: false, message: "Faltan campos requeridos." });
+  }
+
+  if (estado !== 'activo' && estado !== 'inactivo') {
+    return res.status(400).json({ success: false, message: "Estado inválido. Debe ser 'activo' o 'inactivo'." });
+  }
+
+  try {
+    const [result] = await basedatos.query("CALL	SP_ACTUALIZAR_PERMISO_ESTADO(?, ?, ?)", [id, idPermiso, estado]);
+
+    if (result.affectedRows > 0) {
+      res.json({ success: true, message: "Permiso actualizado correctamente" });
+    } else {
+      res.json({ success: false, message: "No se encontró el permiso o no se actualizó" });
+    }
+  } catch (error) {
+    console.error("Error al cambiar el estado del permiso:", error);
+    res.status(500).json({ success: false, message: "Error interno del servidor" });
+  }
+};
+
+
+
 
   
--- app/routes/routes.user.js
+++ app/routes/routes.user.js
@@ -1,6 +1,6 @@
 import Router from "express";
 import { actualizarPoliticasSeguridad, actualizarTiempoIntentos, asignarRolUsuario, bloquearUsuario, crearUsuario, listarPoliticasSeguridad, listarSesiones, listarUser, logueoUsuario, registroInicioSesión } from "../controllers/userController.js";
-import { ActualizarEstado, GETModulosYpermisos } from "../controllers/controller.modulos.permisos.js";
+import { ActualizarEstado, GETModulosYpermisos, GETpermisos } from "../controllers/controller.modulos.permisos.js";
 
 const userRout = Router();
 
@@ -16,6 +16,7 @@ userRout.get("/listar-politicas", listarPoliticasSeguridad);
 userRout.put("/actualizar-tiempo", actualizarTiempoIntentos);
 userRout.put("/estado/:id", bloquearUsuario)
 userRout.get("/modulos", GETModulosYpermisos);
-userRout.post("/estado", ActualizarEstado);
+userRout.put("/modulos", ActualizarEstado);
+userRout.get("/permisos", GETpermisos);
 
 export default userRout;
